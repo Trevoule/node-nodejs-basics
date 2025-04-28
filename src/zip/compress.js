afdfe288c0bf964aa65af8e38cd77861d6a00a2f
Vladislav Reginevich <65953199+Trevoule@users.noreply.github.com>
2025-04-28 21:09:02 +0200

    feat: add zlib subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { currentDirectoryPath } from './utils.js';
+import { pipeline } from 'stream';
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const fileToCompress = currentDirectoryPath('files', 'fileToCompress.txt');
+    const archiveGz = currentDirectoryPath('files', `archive.gz`);
+
+    const source = createReadStream(fileToCompress);
+    const destination = createWriteStream(archiveGz);
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { currentDirectoryPath } from './utils.js';
+
 const decompress = async () => {
-    // Write your code here 
+    const fileToCompress = currentDirectoryPath('files', 'fileToCompress.txt');
+    const archiveGz = currentDirectoryPath('files', `archive.gz`);
+
+    const source = createReadStream(archiveGz);
+    const gunzip = createGunzip();
+    const destination = createWriteStream(fileToCompress);
+
+    try {
+        await pipeline(source, gunzip, destination);
+        console.log('Decompression completed successfully.');
+    } catch (err) {
+        console.error('An error occurred during decompression:', err);
+        process.exitCode = 1;
+    }
 };
 
 await decompress();
