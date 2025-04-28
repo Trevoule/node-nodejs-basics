d881f8cb46b2f2fe4435ffb45283a19b3fbad082
Vladislav Reginevich <65953199+Trevoule@users.noreply.github.com>
2025-04-29 01:48:53 +0200

    feat: add wt subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,42 @@
-const performCalculations = async () => {
-    // Write your code here
+import { Worker } from 'worker_threads';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const currentDirectoryPath = (file) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    return join(__dirname, file);
 };
+
+const CPUS_COUNT = 8;
+
+function createWorker(cpuNo) {
+    return new Promise(async (resolve) => {
+        const workerPath = await currentDirectoryPath('worker.js');
+        const worker = new Worker(workerPath);
+
+        worker.once('message', (message) => {
+            resolve(message);
+            worker.terminate();
+        });
+
+        worker.once('error', (message) => {
+            resolve(message);
+        });
+
+        worker.postMessage(cpuNo + 10);
+    });
+}
+
+const performCalculations = async () => {
+    const workerPromises = [];
+
+    for (let i = 0; i < CPUS_COUNT; i++){
+        workerPromises.push(createWorker(i));
+    };
+
+    const results = await Promise.all(workerPromises);
+    console.log(results);
+    };
 
 await performCalculations();
