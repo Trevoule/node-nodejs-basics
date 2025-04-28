ec8998937dff83f26361b4314505a11143fe03d0
Vladislav Reginevich <65953199+Trevoule@users.noreply.github.com>
2025-04-28 21:08:31 +0200

    feat: add hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import { currentDirectoryPath } from './utils.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileToCalculatePath = currentDirectoryPath('files', 'fileToCalculateHashFor.txt');
+
+    try {
+        const hashValue = await new Promise((resolve, reject) => {
+            const hash = crypto.createHash('sha256');
+            const rs = fs.createReadStream(fileToCalculatePath);
+            rs.on('error', reject);
+            rs.on('data', chunk => hash.update(chunk));
+            rs.on('end', () => resolve(hash.digest('hex')))
+        });
+        console.log(hashValue);
+    } catch (error) {
+    console.error('Error:', error);
+    }    
 };
 
 await calculateHash();
