e93919d489d22723bd83548ae8937057183eb479
Vladislav Reginevich <65953199+Trevoule@users.noreply.github.com>
2025-04-28 21:08:12 +0200

    feat: add cli subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    console.log(args);
+    
+
+    const groupedArgs = args.reduce((acc, _, index) => {
+        if (index % 2 === 0) {
+            acc.push(args.slice(index, index + 2));
+        }
+        return acc;
+    }, []);
+
+    const modifiedArgsString = groupedArgs.map(([key, value]) => {
+        const keyString = key.split('--')[1];
+        return `${keyString} is ${value}`;
+    }).join(', ');    
+    console.log(modifiedArgsString);
 };
 
 parseArgs();
