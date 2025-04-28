df82e2e04441017e5ad5796b8ff482c4672fa9d1
Vladislav Reginevich <65953199+Trevoule@users.noreply.github.com>
2025-04-28 21:08:46 +0200

    feat: add streams subtask

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,28 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
-};
+    const reverseTransform = new Transform({
+        transform(data, _, callback) {
+        const reversed = data.toString().split('').reverse().join('');
+        this.push(reversed);
+        callback();
+        }
+    });
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
+
+    reverseTransform.on('error', (err) => {
+        console.error('Transform stream error:', err.message);
+    });
+
+    process.stdin.on('error', (err) => {
+        console.error('Stdin error:', err.message);
+    
+    });
+    
+    process.stdout.on('error', (err) => {
+        console.error('Stdout error:', err.message);
+    });
+}
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import { currentDirectoryPath } from './utils.js';
+
 const write = async () => {
-    // Write your code here 
+    const fileToWrite = currentDirectoryPath('files', 'fileToWrite.txt');
+
+    const writableStream = fs.createWriteStream(fileToWrite, { encoding: 'utf-8' });
+
+    process.stdin.pipe(writableStream);
+
+    writableStream.on('error', (err) => {
+        console.error('Error writing the file:', err.message);
+    });
+
+    writableStream.on('end', () => {
+        console.log('Finished');
+    });
+
+    // Resume stdin so it starts flowing
+    process.stdin.resume();
 };
 
 await write();
